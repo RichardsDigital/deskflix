e9108f126d3df87f2b211f402ccb02ac3b9c7e15
Daniel Richards <Kordifier@Gmail.com>
2020-06-12 01:18:39 +0100

    Improved qoute banner

--- src/components/QuoteBanner.jsx
+++ src/components/QuoteBanner.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import '../styles/qouteBanner.css';
+import left from '../images/icons/left.png';
+import right from '../images/icons/right.png';
 
 function QuoteBanner() {
     return (
-        <div className="quoteBanner"></div>
+        <div className="quoteBanner">
+            <div className="innerBanner">
+                <img src={left}/>
+                <div className="text">
+                    <p>A great way to keep in touch with new thoughts and ideas</p>
+                    <h4>Steve Morgan, Princibility Building Society</h4>
+                </div>
+                <img src={right}/>
+            </div>
+        </div>
     )
 }
 
