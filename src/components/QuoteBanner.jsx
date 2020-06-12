b10ae1a6abe3b905c1b593b9975218d2785be359
Daniel Richards <Kordifier@Gmail.com>
2020-06-12 13:29:58 +0100

    Added animations

--- src/components/QuoteBanner.jsx
+++ src/components/QuoteBanner.jsx
@@ -7,12 +7,12 @@ function QuoteBanner() {
     return (
         <div className="quoteBanner">
             <div className="innerBanner">
-                <img src={left}/>
+                <img className="left" src={left} alt="left"/>
                 <div className="text">
                     <p>A great way to keep in touch with new thoughts and ideas</p>
                     <h4>Steve Morgan, Princibility Building Society</h4>
                 </div>
-                <img src={right}/>
+                <img className="right" src={right} alr="right"/>
             </div>
         </div>
     )
